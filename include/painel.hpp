d6c5f75fbfcb302089c8f62904b506aaa27609f3
Izabela Tavares <belabh2003yahoo@gmail.com>
2022-11-22 18:34:02 -0300

    painel comentado

--- include/painel.hpp
+++ include/painel.hpp
@@ -6,17 +6,28 @@
 #include <iostream>
 #include "usuario.hpp"
 
+//classe inicial, painel de entrada do app
 class Painel{
     private:
+    //variável para ligar e desligar o app
     bool ativo = false;
 
     public:
+    //liga e desliga o app
     void setAtivo(bool ativo);
+
+    //exibe uma tabela de alimentos, seus valores
+    //nutricionais e respectivos códigos
     void tabelaUniversal();
+
+    //registra um usuario no app
     Usuario* registrarUsuario(std::string nome, int idade, char genero,
                           int altura, int peso, std::string email, int limite);
+
+    //leva ao diário associado ao usuario passado como parâmetro
     void abrirDiario(std::vector<Usuario*> usuarios);
 
+    //destrutor
     ~Painel();
 };
 
--- src/painel.cpp
+++ src/painel.cpp
@@ -2,6 +2,7 @@
 #include <iterator>
 
 void Painel::setAtivo(bool ativo){
+    //exibe mensagem quando o app é fechado
     if(ativo == false){
         std::cout << "Fechando..." << std::endl;
         this->~Painel();
@@ -10,24 +11,30 @@ void Painel::setAtivo(bool ativo){
 }
 
 void Painel::tabelaUniversal(){
-    //implementar leitura do arquivo
+    //implementar leitura do arquivo com as informações
 }
 
 Usuario* Painel::registrarUsuario(std::string nome, int idade, char genero,
                           int altura, int peso, std::string email, int limite){
+    
+    //constroi um objeto usuário
+
     Usuario* usuario = new Usuario (nome,idade,genero,altura,peso,email,limite);
     std::cout << "Usuario registrado" << std::endl; 
+
     return usuario;
 }
 
 void Painel::abrirDiario(std::vector<Usuario*> usuarios){
     
+    //confere se existe pelo menos um usuário registrado
     if(!usuarios.size()){
         std::cout << "Não existem usuários cadastrados" << std::endl
         << "Crie um novo usuário para acessar o diário" << std::endl;
         return;
     }
 
+    //procura ocorrência do usuario digitado nos registros
     bool achou = false;
     while(!achou){
         std::cout << "Digite o usuário desejado: ";
@@ -35,16 +42,20 @@ void Painel::abrirDiario(std::vector<Usuario*> usuarios){
         std::cin >> nomeUsuario;
 
         for(auto it = usuarios.begin(); it != usuarios.end(); it++){
+            //caso acha o usuario, exibe entradas de seu diario
             if((*it)->getNome() == nomeUsuario){
                 (*it)->getDiario()->mostrarEntradas();
                 achou = true;
             }
+
+            //caso não acha pergunta se quer procurar outro usuario
             if(!achou){
                 std::cout << "Usuario nao encontrado" << std::endl;
                 std::cout << "Digitar novo usuario? S/N" << std::endl;
                 char caso;
                 std::cin >> caso;
                 if(caso == 'N'){
+                    std::cout << "Voltando para o painel inicial..." << std::endl;
                     return;
                 }
             }
